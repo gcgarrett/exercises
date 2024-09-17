3e9a55efea2e5e457100051a4ce95812489e339e
Gregory Garrett <gcgarrett@gmail.com>
2024-09-17 17:54:43 -0400

    Updated Go sourcecode to use spaces instead of tabs

--- waysToScore/waysToScore.go
+++ waysToScore/waysToScore.go
@@ -3,28 +3,28 @@ package main
 import "fmt"
 
 func waysToScorePoints(totalPoints int, possiblePoints []int) int {
-	if totalPoints < 0 || len(possiblePoints) == 0 {
-		return 0
-	} else if totalPoints == 0 {
-		return 1
-	}
+    if totalPoints < 0 || len(possiblePoints) == 0 {
+        return 0
+    } else if totalPoints == 0 {
+        return 1
+    }
 
-	currentPoints := possiblePoints[0]
+    currentPoints := possiblePoints[0]
 
-	return waysToScorePoints(totalPoints - currentPoints, possiblePoints) +
-			waysToScorePoints(totalPoints, possiblePoints[1:])
+    return waysToScorePoints(totalPoints - currentPoints, possiblePoints) +
+            waysToScorePoints(totalPoints, possiblePoints[1:])
 }
 
 func waysToScore(totalPoints int) int {
-	possiblePoints := []int{6, 3, 2}
+    possiblePoints := []int{6, 3, 2}
 
-	return waysToScorePoints(totalPoints, possiblePoints)
+    return waysToScorePoints(totalPoints, possiblePoints)
 }
 
 func main() {
-	testValues := [6]int{5, 12, 6, 11, 59, 24}
+    testValues := [6]int{5, 12, 6, 11, 59, 24}
 
-	for _, testValue := range testValues {
-		fmt.Printf("waysToScore(%d)\n%d\n", testValue, waysToScore(testValue))
-	}
+    for _, testValue := range testValues {
+        fmt.Printf("waysToScore(%d)\n%d\n", testValue, waysToScore(testValue))
+    }
 }
